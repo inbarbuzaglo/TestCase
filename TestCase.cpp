7c6c8bbdd67852ec3a911c815dd507593b1fc064
Meytal Levin <33618164+Meyt3893@users.noreply.github.com>
2018-06-07 09:53:04 +0300

    new code

---
+++ TestCase.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "TestCase.hpp"
+using namespace std;
+
+
+   TestCase::TestCase(string name, ostream& out)
+   {
+       this->name=name;
+
+       //should define the ostream as class member and initiliize it
+   }
+
+
+   template<typename T> TestCase& TestCase::check_equal(T ob1,T ob2)const
+   {
+       counter++;
+       if(ob1!=ob2)
+       {
+           this->out<<"Failure in test"<<counter<<"the parameters is not equal!";
+       }
+         retrun &this;     
+   }
+
+   
+
+ 
---
+++ TestCase.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+static int counter=0;
+
+
+class TestCase{
+   
+    string name;
+    ostream& out;
+  
+    public:    
+    TestCase(string name, ostream& out);
+     template<typename T> TestCase& check_equal(T ob1,T ob2)const;
+    
+};
