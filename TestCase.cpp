9faa8ec788c152c6926009bd061d09addf9f50d0
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-06-08 20:22:47 +0300

    Update TestCase.cpp

--- TestCase.cpp
+++ TestCase.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 #include <string>
+#include <functional>
 #include "TestCase.hpp"
 using namespace std;
 
+//כשניסיתי לכתוב בנאי בצורה הזו , לא קיבלתי שגיאה 
 
-   TestCase::TestCase(string name, ostream& out)
-   {
-       this->name=name;
-
-       //should define the ostream as class member and initiliize it
-   }
-
+TestCase::TestCase(string test, ostream& outStream) :  name(test) , out(outStream) {}
 
    template<typename T> TestCase& TestCase::check_equal(T ob1,T ob2)const
    {
@@ -32,11 +28,33 @@ using namespace std;
         retrun &this;     
    }
 
-   /** template<typename T> TestCase& TestCase::check_function(,T ob1,T ob2)const
+   template<typename T,typename Function,typename Arg> TestCase& TestCase::check_function(Function f,Arg arg,T ob2)
     {
+        counter++;
+        T func=f(arg);//פונקציה מקבלת arg
+        if(func!=ob2)
+        {
+
+			this->out<<"Function should return "<<ob2<<" but returned"<<arg<<"!";
+        }
         return &this;
     }
-    **/
-   
+    template<typename T> TestCase& TestCase::check_output(T ob1,string name)
+   {
+       counter++;
+       //המרה לסטרינג
+       ostringstream b1;
+       b1<<ob1;
+       if(b1.str()!=name)
+       {
+             this->out<<"string value should be"<<name<<" but is"<<b1.str();
+       }
+       return &this;
 
- 
+   }
+   void TestCase::print()
+{ 
+      this->out << counter << "failed," << endl;
+    this->out << counter << " passed," << endl;
+	this->out << counter << " total." << endl;
+}
