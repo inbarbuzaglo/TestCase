4411c90e1d7156556924e0c40e6b6c94a9b33315
InbarBuzaglo <33480360+inbarbuzaglo@users.noreply.github.com>
2018-06-08 20:23:33 +0300

    Update TestCase.hpp

--- TestCase.hpp
+++ TestCase.hpp
@@ -1,6 +1,8 @@
 #pragma once
-#include <iostream>
 #include <string>
+#include <functional>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -13,8 +15,11 @@ class TestCase{
     ostream& out;
   
     public:    
-    TestCase(string name, ostream& out);
+    TestCase(string test, ostream& outStream);
     template<typename T> TestCase& check_equal(T ob1,T ob2)const;
     template<typename T> TestCase& check_different(T ob1,T ob2)const;
-    template<typename T> TestCase& check_function(void*,T ob1,T ob2)const;
+    template<typename T,typename Function,typename Arg> TestCase& check_function(Function f,Arg arg,T ob2);
+    template<typename T> TestCase& check_output(T ob1,string name);
+    void print();
+
 };
